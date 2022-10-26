9a625de6b934819ced9770873c09a87b6a480c8b
aisenurzeren <80218995+aisenurzeren@users.noreply.github.com>
2022-10-26 11:54:13 +0300

    Add files via upload

---
+++ görüntü işleme/histogram.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+image_name = "manzara.jpg"
+img = cv2.imread(image_name) 
+
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+size_y = img.shape[0] 
+size_x = img.shape[1]
+
+flattened = img.reshape([size_x*size_y]) 
+
+from matplotlib import pyplot as plt 
+
+rhist,_ ,_ = plt.hist(flattened, bins=8)
+plt.show() 
